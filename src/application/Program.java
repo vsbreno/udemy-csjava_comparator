c73d96e488865b54e6df3bfe1bd5cb68f031ccce
Breno Santos <brenovinicius.santos0@gmail.com>
2024-11-18 19:43:20 -0300

    Interface comparator to order a list

---
+++ src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		// 1º forma
+		//list.sort(new MyComparator());
+		
+		//2º forma
+		/*Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+		*/
+		
+		//3ª forma
+		/*Comparator<Product> comp = (p1,p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		};
+		*/
+		
+		//4º forma
+		//Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
+		for (Product p: list) {
+			System.out.println(p);
+		}
+	}
+
+}
